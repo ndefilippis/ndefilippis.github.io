08e172327e3454e72c4892e0d58634ec0a247767
radicalonion <ndefilippis98@gmail.com>
2016-01-21 14:07:44 -0500

    Add team info

--- js/animate.js
+++ js/animate.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
     $(this).find(".caption:first").animate({
       height: '+=150px'
     });
+    $(this).find(".info").css("display: inline-block");
   });
   $(".card").mouseleave(function(){
     $(this).find(".caption:first").animate({
       height: '-=150px'
     });
+    $(this).find(".info").css("display: none");
   });
 });
